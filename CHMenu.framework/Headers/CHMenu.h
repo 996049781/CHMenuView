471e18465bbf175c9e870f790c22a7330c0a671a
chenghao <chenghao@chenghaodeMacBook-Pro.local>
2017-02-28 18:50:58 +0800

    first

---
+++ CHMenu.framework/Headers/CHMenu.h
@@ -0,0 +1,19 @@
+//
+//  CHMenu.h
+//  CHMenu
+//
+//  Created by chenghao on 2017/2/28.
+//  Copyright © 2017年 clearcdm.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CHMenu.
+FOUNDATION_EXPORT double CHMenuVersionNumber;
+
+//! Project version string for CHMenu.
+FOUNDATION_EXPORT const unsigned char CHMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHMenu/PublicHeader.h>
+
+#import <CHMenu/CHMenuView.h>
